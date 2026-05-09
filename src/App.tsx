bd9d956220dd2d75c9f34758e865bfa3902507a3
Ravaly23 <valeryravaly2003@gmail.com>
2026-05-09 05:24:08 +0300

    Route Frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
-import ProfilSeller from "./Pages/ProfilSeller"
-import Home from "./Pages/Home"
-import Auth from "./Pages/Authentification"
+import Dashboard from './Pages/Dashboard'
+import { createBrowserRouter } from "react-router-dom";
+import AddArticle from './Pages/AddArticle';
+import Accueil from './Pages/Accueil';
+import Auth from './Pages/Authentification';
+import Home from './Pages/Home';
+import ProfilSeller from './Pages/ProfilSeller';
+
+const router = () => {
+     return (createBrowserRouter([
+  {
+    path: "/",
+    Component: Accueil,
+  },
+  {
+    path: "/profilxxxx/Add",
+    Component:AddArticle,
+  },
+  {
+    path:"/profilxxxx/dashboard",
+    Component: Dashboard
+  },{
+    path:"/auth",
+    Component:Auth,
+  },{
+    path:"/profilxxxx/home",
+    Component:Home
+  },{
+    path:"/profilxxxx",
+    Component: ProfilSeller
+  }
+]));
+}
 
 function App() {
-    return (<Auth />)
+    return router();
 }
 
 export default App
